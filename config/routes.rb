1282a62a932dbfbc8f642d34358a9dbfb162d9d3
Chris Severns <severnsc@gmail.com>
2016-08-09 17:42:54 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   get 'contact', to: 'static_pages#contact'
   
   get 'signup', to: 'users#new'
+  
+  post 'signup', to: 'users#create'
 
   root "static_pages#home"
+  
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+ test "invalid signup form information" do
+   get signup_path
+    assert_no_difference 'User.count' do
+      assert_select 'form[action="/signup"]'
+      post signup_path, params: { user: { name: "",
+                                          email: "invalid@example",
+                                          password: "foo",
+                                          password_confirmation: "bar"
+      }}
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation"
+    assert_select "div.alert-danger"
+ end
+
+test "valid signup form information" do
+ get signup_path
+ assert_difference 'User.count', 1 do
+  post users_path, params: { user: { name: "Example User",
+                                     email: "user@example.com",
+                                     password: "password",
+                                     password_confirmation: "password"} }
+  end
+  follow_redirect!
+  assert_template 'users/show'
+  assert_select 'div.alert'
+ end
+end
